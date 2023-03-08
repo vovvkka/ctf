a25d95bbb9b176e8cd00e4a62428d16bf6389f7b
Vladimir Baltiev <v.golem2288@gmail.com>
2023-03-09 00:34:34 +0600

    Added footer

--- frontend/src/components/UI/Layout/Layout.js
+++ frontend/src/components/UI/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "../Header/Header";
+import Footer from "../../Footer/Footer";
 const Layout = ({children}) => {
 
     return (
@@ -8,6 +9,7 @@ const Layout = ({children}) => {
             <div className='main'>
                     {children}
             </div>
+            <Footer/>
         </div>
     );
 };
