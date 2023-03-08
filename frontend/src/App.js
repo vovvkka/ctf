76d64765a9093fe4c82abeec2b366c628e8bbcb8
Vladimir Baltiev <v.golem2288@gmail.com>
2023-03-09 01:51:50 +0600

    Added register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Layout from "./components/UI/Layout/Layout";
 import {Route, Switch} from "react-router-dom";
 import HomePage from "./containers/HomePage";
 import Login from "./containers/Login";
+import Register from "./containers/Register";
 
 const App = () => {
     return (
@@ -10,6 +11,7 @@ const App = () => {
             <Switch>
                 <Route path="/" component={HomePage} exact />
                 <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
             </Switch>
         </Layout>
     );
---
+++ frontend/src/containers/Register.js
@@ -0,0 +1,162 @@
+import React, {useState} from 'react';
+import close from "../assets/close-eye.png";
+import open from "../assets/open-eye.png";
+import {Link} from "react-router-dom";
+
+const Register = () => {
+    const [user, setUser] = useState({
+        username: [""],
+        lastname: [""],
+        teamName: "",
+        email: "",
+        password: "",
+    });
+
+    const [isShow, setIsShow] = useState(false);
+
+    const multipleChangeHandler = (e, index) => {
+        const { name, value } = e.target;
+
+        setUser((prev) => {
+            const newArr = prev[name].map((item, i) => {
+                if (index === i) {
+                    return value;
+                }
+
+                return item;
+            });
+
+            return {
+                ...prev,
+                [name]: newArr,
+            };
+        });
+    };
+
+    const inputUserChangeHandler = e => {
+        const {name, value} = e.target;
+        setUser(prev => ({...prev, [name]: value}));
+    };
+
+    const addInputHandler = () => {
+        setUser((prev) => ({
+            ...prev,
+            username: [...prev["username"], ''],
+            lastname: [...prev["lastname"], ""]
+        }));
+    };
+
+    return (
+        <div className="register">
+            <div className="register__page">
+                <p>Register your team!</p>
+            </div>
+
+            <div className="container-xs">
+                <div className="register__block">
+                    {
+                        user.username?.map((us, idx) => (
+                            <div className="register__row-block" key={us + idx}>
+                                <div className="register__input-block-row">
+                                    <label>User {idx + 1}*</label>
+                                    <input
+                                        name="username"
+                                        className="register__input"
+                                        value={user.username[idx]}
+                                        onChange={e => multipleChangeHandler(e, idx)}
+                                        placeholder="First Name"
+                                        autoFocus
+                                        required
+                                    />
+                                </div>
+
+                                <div className="register__input-block-row">
+                                    <label>Last Name*</label>
+                                    <input
+                                        name="lastname"
+                                        className="register__input"
+                                        value={user.lastname[idx]}
+                                        onChange={e => multipleChangeHandler(e, idx)}
+                                        placeholder="Last Name"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        ))
+                    }
+
+
+                    <div className="register__input-block">
+                        <label>Team Name</label>
+                        <input
+                            name="teamName"
+                            className="register__input"
+                            value={user.teamName}
+                            onChange={inputUserChangeHandler}
+                        />
+                    </div>
+
+                    <div className="register__input-block">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            autoComplete="off"
+                            className="register__input"
+                            value={user.email}
+                            onChange={inputUserChangeHandler}
+                        />
+                    </div>
+
+                    <div className="register__input-block">
+                        <label>Password</label>
+                        <div className="register__eye-block">
+                            <input
+                                type={isShow ? "text" : "password"}
+                                name="password"
+                                autoComplete="off"
+                                className="register__input"
+                                value={user.password}
+                                onChange={inputUserChangeHandler}
+                            />
+                            <img
+                                src={isShow ? close : open}
+                                onClick={() => setIsShow(prev => !prev)}
+                                className="register__input-eye"
+                                width="30px"
+                                alt="eye"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="register__add-user">
+                        <button
+                            className="register__add-user-btn"
+                            disabled={user.username.length > 4}
+                            onClick={addInputHandler}
+                        >
+                            Add another member
+                        </button>
+                    </div>
+
+                    <div className="register__buttons">
+                        <Link to="/">
+                            <button
+                                className="register__btn register__btn-cancel"
+                            >
+                                Cancel
+                            </button>
+                        </Link>
+                        <button
+                            className="register__btn register__btn-accept"
+                        >
+                            Register
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
